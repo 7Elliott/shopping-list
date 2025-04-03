9491824209feca219bfed5a918ad311d2c09806f
hulloanson <hulloanson@users.noreply.github.com>
2025-04-03 02:42:21 +0000

    deploy: 595d8fdf44bca18ca3153491ba89bc337369e50f

---
+++ login.js
@@ -0,0 +1,20 @@
+const auth = new Auth()
+
+async function handleLoginSubmit(e) {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const username = formData.get('username')
+    const password = formData.get('password')
+    console.log(username, password)
+    const { error } = await auth.signIn(username, password)
+    if (error) {
+        alert('Failed to login. Check your username / password, or try again.')
+        return
+    }
+    window.location.pathname = '/'
+    return
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    document.getElementById("loginForm").addEventListener("submit", handleLoginSubmit);
+});
